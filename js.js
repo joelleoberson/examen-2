40c3bd86101fb607e74c084f786a40a5fd7ffc4c
joelleoberson <joelle.oberson@studentfr.ch>
2023-11-27 14:54:26 +0100

    animation burger menu + ajouts d'icons

--- js.js
+++ js.js
@@ -1,13 +1,18 @@
 var burger = document.querySelector(".burger");
 var menu = document.querySelector(".menu");
+var menuLinks = document.querySelectorAll(".menu a");
 
-var toggleMenu = function toggleMenu() {
+var toggleMenu = function () {
   menu.classList.toggle("is-active");
+  burger.classList.toggle("is-active");
 };
 
 burger.addEventListener("click", toggleMenu);
 
-var toggleMenu = function toggleMenu() {
-  menu.classList.toggle("is-active");
-  burger.classList.toggle("is-active");
-};
+// Ajouter un gestionnaire d'événements pour chaque lien dans le menu
+menuLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    // Fermer le menu lorsque le lien est cliqué
+    toggleMenu();
+  });
+});
